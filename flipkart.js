46004092a20a781f4167dafa1a798fae9f197b27
sagar raj yadav <115137591+sagar-raj-yadav@users.noreply.github.com>
2023-08-08 05:23:23 +0530

    Add files via upload

---
+++ flipkart.js
@@ -0,0 +1,20 @@
+function scroll_first_section_left(){
+    var left = document.querySelector('.sub_item')
+    left.scrollBy(350,0)
+}
+
+function scroll_second_section_left(){
+    var left = document.querySelector('.sub_item5')
+    left.scrollBy(350,0)
+}
+
+function scroll_first_section_right(){
+    var right = document.querySelector('.sub_item')
+    right.scrollBy(-350,0)
+}
+
+function scroll_second_section_right(){
+    var right = document.querySelector('.sub_item5')
+    right.scrollBy(-350,0)
+}
+// scroll bar end
